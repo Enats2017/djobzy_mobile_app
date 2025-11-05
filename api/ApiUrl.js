0c376c1249d0c5cf69224693afdf6a91955ce237
Swayam Parmar <swayam.enats@gmail.com>
2025-11-05 14:59:35 +0530

    api url updated

--- api/ApiUrl.js
+++ api/ApiUrl.js
@@ -1,5 +1,5 @@
 
-export const API_URL = "http://192.168.10.109:8000/api";
-export const API_ICON = "http://192.168.10.109:8000";
-// export const API_URL = "https://test.djobzy.com/api";
-// export const API_ICON = "https://test.djobzy.com";
+// export const API_URL = "http://192.168.10.109:8000/api";
+// export const API_ICON = "http://192.168.10.109:8000";
+export const API_URL = "https://test.djobzy.com/api";
+export const API_ICON = "https://test.djobzy.com";
